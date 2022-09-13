4bdfc0ad4b44446568fd092394e33927ab1fdccc
topguns837 <kharjun4@gmail.com>
2022-09-14 01:58:54 +0530

    final demo video

--- main.py
+++ main.py
@@ -20,6 +20,18 @@ filename = "input.jpg"
 
 st.header("Myntra Fashion Recommender")
 
+sex = None
+
+sex = st.radio(
+"Enter your gender : ",
+('Male', 'Female'))
+
+if sex == "Male" :
+    sex = "Men"
+else :
+    sex = "Women"
+
+
 def save_uploadedfile(uploadedfile):
     global filename
     #filename = uploadedfile.name
@@ -43,138 +55,146 @@ if image_file is not None :
     #print(image_path)    
 
     if ps.driver(image_path) :
-        st.write("Full shot")
+        #st.write("Full shot")
         driver('temp/input.jpg')
         yolo_output = Image.open('temp/yolo_output.jpg')
         st.image(yolo_output, caption='Input Image')
 
         data_json = pd.read_csv(os.path.join('Product', 'data_json_new_copy.csv'))
         print(data_json.columns)
+
         
-        for file in os.listdir('user_products') :
-            print("for loop")
-            new_embedding = model_definition.extract_features(os.path.join('user_products', file), model_definition.model)
-            new_embedding_arr = []
+        #print(sex)
 
-            for i in range(len(new_embedding)) :
-                new_embedding_arr.append(new_embedding[i])
+        if True:
 
-            new_embedding = np.array(new_embedding)
+        
+            for file in os.listdir('user_products') :
+                print("for loop")
+                new_embedding = model_definition.extract_features(os.path.join('user_products', file), model_definition.model)
+                new_embedding_arr = []
 
-            scaler = StandardScaler()
-            new_feature_scaled = scaler.fit_transform(new_embedding.reshape(-1,1))
-            new_feature_scaled = np.array(new_feature_scaled).squeeze()
+                for i in range(len(new_embedding)) :
+                    new_embedding_arr.append(new_embedding[i])
 
-            old_embedding = np.load('Product/features_pca.pkl', allow_pickle = True)
-            old_features = np.load('Product/features_np_2.pkl', allow_pickle = True)
+                new_embedding = np.array(new_embedding)
 
-            topwear_features = old_features[0:187]
-            bottomwear_features = old_features[187 : 387]
-            footwear_features = old_features[387 : 590]
-            eyewear_features = old_features[591 : 792]
-            handbag_features = old_features[793 : 994]
+                scaler = StandardScaler()
+                new_feature_scaled = scaler.fit_transform(new_embedding.reshape(-1,1))
+                new_feature_scaled = np.array(new_feature_scaled).squeeze()
 
-            base_index = 0
+                old_embedding = np.load('Product/features_pca.pkl', allow_pickle = True)
+                old_features = np.load('Product/features_np_2.pkl', allow_pickle = True)
 
-            neighbors, distances, indices = [], [], []
+                topwear_features = old_features[0:187]
+                bottomwear_features = old_features[187 : 387]
+                footwear_features = old_features[387 : 590]
+                eyewear_features = old_features[591 : 792]
+                handbag_features = old_features[793 : 994]
 
-            sex = 'Women'
-            base_index = 0
-            
-            if file == "Topwear.jpg" :
-                features = data_json[(data_json['category'] == 'topwear') & (data_json['sex']==sex)]['features'].tolist()
-                #features = data_json[(data_json['category'] == 'topwear') & (data_json['sex']=='Women')]['features'].tolist()
-
-                #neighbors = NearestNeighbors(n_neighbors=8, algorithm='brute', metric='euclidean').fit(list(features))
-                #distances, indices = neighbors.kneighbors([new_feature])
-
-                neighbors = NearestNeighbors(n_neighbors=15, algorithm='brute', metric='euclidean').fit(list(topwear_features))
-                distances, indices = neighbors.kneighbors([new_feature_scaled])
                 base_index = 0
-                st.header("\n Topwear Recommendations \n")
-            
-            elif file == "Bottomwear.jpg" :
-                features = data_json[(data_json['category'] == 'bottomwear') & (data_json['sex']==sex)]['features'].tolist()
 
-                neighbors = NearestNeighbors(n_neighbors=15, algorithm='brute', metric='euclidean').fit(list(bottomwear_features))
-                distances, indices = neighbors.kneighbors([new_feature_scaled])
-                base_index = 187
-                st.header("\n Bottomwear Recommendations \n")
+                neighbors, distances, indices = [], [], []
 
-            elif file == "Footwear.jpg" :
-                features = np.array(data_json[(data_json['category'] == 'footwear') & (data_json['sex']==sex)]['features'].tolist())
-                print(features.shape)
+                #sex = 'Women'
+                base_index = 0
 
-                #try :
-                neighbors = NearestNeighbors(n_neighbors=15, algorithm='brute', metric='euclidean').fit(footwear_features)
-                distances, indices = neighbors.kneighbors([new_feature_scaled])
-                base_index = 387
-                st.header("\n Footwear Recommendations \n")
-                #except :
-                    #pass
+                if file == "Topwear.jpg" :
+                    features = data_json[(data_json['category'] == 'topwear') & (data_json['sex']==sex)]['features'].tolist()
+                    #features = data_json[(data_json['category'] == 'topwear') & (data_json['sex']=='Women')]['features'].tolist()
 
-            elif file == "Handbag.jpg" :
-                features = data_json[(data_json['category'] == 'handbag') & (data_json['sex']==sex)]['features'].tolist()
+                    #neighbors = NearestNeighbors(n_neighbors=8, algorithm='brute', metric='euclidean').fit(list(features))
+                    #distances, indices = neighbors.kneighbors([new_feature])
 
-                eighbors = NearestNeighbors(n_neighbors=15, algorithm='brute', metric='euclidean').fit(list(handbag_features))
-                distances, indices = neighbors.kneighbors([new_feature_scaled])
-                base_index = 792
-                st.header("\n Handbag Recommendations \n")
+                    neighbors = NearestNeighbors(n_neighbors=15, algorithm='brute', metric='euclidean').fit(list(topwear_features))
+                    distances, indices = neighbors.kneighbors([new_feature_scaled])
+                    base_index = 0
+                    st.header("\n Topwear Recommendations \n")
 
-            else:
-                pass
+                elif file == "Bottomwear.jpg" :
+                    features = data_json[(data_json['category'] == 'bottomwear') & (data_json['sex']==sex)]['features'].tolist()
 
+                    neighbors = NearestNeighbors(n_neighbors=15, algorithm='brute', metric='euclidean').fit(list(bottomwear_features))
+                    distances, indices = neighbors.kneighbors([new_feature_scaled])
+                    base_index = 187
+                    st.header("\n Bottomwear Recommendations \n")
 
+                elif file == "Footwear.jpg" :
+                    features = np.array(data_json[(data_json['category'] == 'footwear') & (data_json['sex']==sex)]['features'].tolist())
+                    print(features.shape)
 
-            indexes = indices
-            #print(indexes)
+                    #try :
+                    neighbors = NearestNeighbors(n_neighbors=15, algorithm='brute', metric='euclidean').fit(footwear_features)
+                    distances, indices = neighbors.kneighbors([new_feature_scaled])
+                    base_index = 387
+                    st.header("\n Footwear Recommendations \n")
+                    #except :
+                        #pass
 
-            root_dir = 'Product'
+                elif file == "Handbag.jpg" :
+                    features = data_json[data_json['category'] == 'handbag']['features'].tolist()
 
-            filenames = []
-            category = data_json.iloc[indices[0],:]['category']
+                    eighbors = NearestNeighbors(n_neighbors=15, algorithm='brute', metric='euclidean').fit(list(handbag_features))
+                    distances, indices = neighbors.kneighbors([new_feature_scaled])
+                    base_index = 792
+                    st.header("\n Handbag Recommendations \n")
 
-            names = []
-            prices = []
-            links = []
-
-            for index in indexes[0] :
-                img_path = os.path.join(root_dir, data_json.iloc[index + base_index,:]['category'], data_json.iloc[index + base_index,:]['name']) + '.jpg'
-                filenames.append(img_path)
-                if data_json.iloc[index + base_index,:]['sex']==sex :
-
-                    links.append(data_json.iloc[index + base_index,:]['link'])
-                    names.append(data_json.iloc[index + base_index, :]['name'])
-                    prices.append(data_json.iloc[index + base_index, : ]['price'])
-
-                if len(links)>=5:
-                    break
-
-            #print(filenames)
-
-            counter = 0
-
-            #try :
-
-            for img_path in filenames :
-                try :
-
-                    #img = cv2.imread(img_path)
-                    img = Image.open(img_path)
-                    #img = img.resize((600, 400))
-                    name, link = names[counter], links[counter]
-                    st.image(img, caption='test', width  = 200)
-                    st.write(name)
-                    st.write("Price : " +  str(prices[counter]) + " \u20B9")
-                    st.write(link)
-                    counter += 1
-                    if counter>=5 :
-                        break
-                except :
+                else:
                     pass
 
-            #except :
-                #pass
+
+
+                indexes = indices
+                #print(indexes)
+
+                root_dir = 'Product'
+
+                filenames = []
+                category = data_json.iloc[indices[0],:]['category']
+
+                names = []
+                prices = []
+                links = []
+
+                for index in indexes[0] :
+                    if data_json.iloc[index + base_index,:]['sex']==sex :
+                        img_path = os.path.join(root_dir, data_json.iloc[index + base_index,:]['category'], data_json.iloc[index + base_index,:]['name']) + '.jpg'
+                        filenames.append(img_path)
+                    
+
+                        links.append(data_json.iloc[index + base_index,:]['link'])
+                        names.append(data_json.iloc[index + base_index, :]['name'])
+                        prices.append(data_json.iloc[index + base_index, : ]['price'])
+
+                    if len(links)>=5:
+                        break
+
+                #print(filenames)
+
+                counter = 0
+
+                #try :
+                index = 0
+                for img_path in filenames :
+                    try :
+
+                        
+                        img = Image.open(img_path)
+                        
+                        index = filenames.index
+                        name, link = names[counter], links[counter]
+                        st.image(img, caption='test', width  = 200)
+                        st.write(name)
+                        st.write("Price : " +  str(prices[counter]) + " \u20B9")
+                        st.write(link)
+                        counter += 1
+                        if counter>=5 :
+                            break
+                    except :
+                        pass
+
+                #except :
+                    #pass
 
 
 
